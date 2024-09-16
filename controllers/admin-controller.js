542f6dc8ceb9e4546c234c68944c96d634655777
brownishgreen <jjjjjimyuan@gmail.com>
2024-09-16 17:36:21 +0800

    feat:integrate Imgur API for restaurant image upload

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,5 @@
 const { Restaurant } = require('../models')
-const { localFileHandler } = require('../helpers/file-helpers')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -16,15 +16,17 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req
-    localFileHandler(file)
-      .then(filePath => Restaurant.create({
-        name,
-        tel,
-        address,
-        openingHours,
-        description,
-        image: filePath || null
-      }))
+    imgurFileHandler(file)
+      .then(filePath => {
+        return Restaurant.create({
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image: filePath || null
+        })
+      })
       .then(() => {
         req.flash('success_messages', 'Restaurant was successfully created!') // 在畫面顯示成功提示
         res.redirect('/admin/restaurants') // 新增完成後導回後台首頁
@@ -57,7 +59,7 @@ const adminController = {
     const { file } = req
     Promise.all([
       Restaurant.findByPk(req.params.id),
-      localFileHandler(file)
+      imgurFileHandler(file)
     ])
       .then(([restaurant, filePath]) => {
         if (!restaurant) throw new Error('Restaurant did not exist!')
--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,14 +1,32 @@
-const fs = require('fs') // 引入 fs 模組
-const localFileHandler = file => { // file 是 multer 處理完的檔案
+const fs = require('fs')
+const axios = require('axios')
+const FormData = require('form-data')
+
+const imgurClientId = '205d9ccedcd3c18' // 從 Imgur API 獲取的 Client ID
+
+const imgurFileHandler = file => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
-    const fileName = `upload/${file.originalname}`
-    return fs.promises.readFile(file.path)
-      .then(data => fs.promises.writeFile(fileName, data))
-      .then(() => resolve(`/${fileName}`))
-      .catch(err => reject(err))
+
+    const form = new FormData()
+    form.append('image', fs.createReadStream(file.path)) // 讀取上傳的圖片檔案
+
+    axios.post('https://api.imgur.com/3/image', form, {
+      headers: {
+        Authorization: `Client-ID ${imgurClientId}`,
+        ...form.getHeaders() // 確保請求標頭包含表單數據
+      }
+    })
+      .then(response => {
+        const { link } = response.data.data // 從 Imgur API 回傳的結果中取得圖片 URL
+        resolve(link) // 返回圖片的 URL
+      })
+      .catch(err => {
+        reject(err) // 處理上傳過程中的錯誤
+      })
   })
 }
+
 module.exports = {
-  localFileHandler
+  imgurFileHandler
 }
