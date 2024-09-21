6a3d8f1140af68d9faae27127d90d5c0f54d51b2
Jing Yuan WU <jjjjjimyuan@gmail.com>
2024-09-21 21:40:45 +0800

    feat: add feeds page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,7 +1,16 @@
 const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
+
+// 啟用 dayjs 的相對時間功能
+dayjs.extend(relativeTime)
+
 module.exports = {
   currentYear: () => dayjs().year(),
+
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
-  }
+  },
+
+  // 把這裡命名為 timeAgo
+  relativeTimeFromNow: a => dayjs(a).fromNow()
 }
